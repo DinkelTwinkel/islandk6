2eeac1c095803e00e3b4fd9770046ebaaf7c5b6e
DinkelTwinkel <128545374+DinkelTwinkel@users.noreply.github.com>
2024-02-20 07:15:31 +0000

    Update DangerRoleColourShifting.js

--- patterns/DangerRoleColourShifting.js
+++ patterns/DangerRoleColourShifting.js
@@ -2,10 +2,45 @@ const KimoTracker = require('../models/kimoTracker');
 const { ActivityType } = require('discord.js');
 
 module.exports = async (client) => {
+  
+  let result = await KimoTracker.findOne({ serverId: '1193663232041304134' });
+
+  console.log ('cutoff clock');
+
+  const millisecondsInDay = 24 * 60 * 60 * 1000;
+
+  const currentDate = new Date();
+  const nextUTCDay = new Date(currentDate.getTime() + millisecondsInDay);
+  nextUTCDay.setHours(12);
+  nextUTCDay.setMinutes(0);
+  nextUTCDay.setSeconds(0);
+  nextUTCDay.setDate(result.nextDate);
+
+  const differenceMiliUTC = result.nextDate - currentDate.getTime();
+
+  console.log (differenceMiliUTC);
+  console.log (currentDate.getTime());
+  console.log (nextUTCDay);
+
+  const differenceSeconds = differenceMiliUTC / 1000;
+  const differenceMinutes = Math.floor( differenceSeconds / 60);
+
+  const colourModifier = differenceMinutes / 1440;
+
+  const roleColour = shiftColor('#e80f00','#ffd65c',colourModifier)
+
+  console.log('current hex: ' + roleColour);
+
+  // find role and change role.
+
+  const kimoServer = await client.guilds.fetch ('1193663232041304134');
+  const DangerRole = kimoServer.roles.cache.get('1202533924040081408');
+
+  DangerRole.setColor(roleColour);
 
 // every x amount of time, change danger role colour.
 setInterval(async () => {
-    const result = await KimoTracker.findOne({ serverId: '1193663232041304134' });
+    result = await KimoTracker.findOne({ serverId: '1193663232041304134' });
 
     console.log ('cutoff clock');
 
