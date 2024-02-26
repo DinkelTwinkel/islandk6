fe4502686b60d281cde410d56fff4db0f8f7e4e6
DinkelTwinkel <128545374+DinkelTwinkel@users.noreply.github.com>
2024-02-26 21:03:05 +0000

    Update jailAuto.js

--- patterns/jailAuto.js
+++ patterns/jailAuto.js
@@ -45,7 +45,7 @@ module.exports = async (client) => {
 
         const messageLowercase = message.content.toLowerCase();
 
-        const bannedWords = ["retard", "andrew's underwear", "premo", "boney", "zuckerberg", "shemale", "boku no pico", "league of legends", "faggot", "tranny", "fag", "lesbo", "dyke", "stocks", "stock", "ai"];
+        const bannedWords = ["retard", "andrew's underwear", "premo", "boney", "zuckerberg", "shemale", "boku no pico", "league of legends", "faggot", "tranny", "fag", "lesbo", "dyke", "stocks", "stock"];
         const replacementWord = ["sweetiepie", "JASON IS AWESOME", "i have found jesus", "honey darling", "my bum is itchy", "my love", "bby", "habibi", '[REDACTED]', '♥'];
 
         bannedWords.forEach( async element => {
