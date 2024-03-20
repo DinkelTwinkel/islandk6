755e2b61e0b7fc72b3816b4d9e1cb00978613300
DinkelTwinkel <128545374+DinkelTwinkel@users.noreply.github.com>
2024-03-20 20:17:29 +0000

    Update smuggle.js

--- commands/smuggle.js
+++ commands/smuggle.js
@@ -32,10 +32,19 @@ module.exports = {
             announcementChannel = interaction.guild.channels.cache.get('1202876942714544148');
             await announcementChannel.send(`${interaction.member} has been smuggled over...`);
           }
-          else if (interaction.member.roles.cache.get('1202749571957006348')) {
-
+          else if (userWallet.group === 1) {
+            //group b
+            interaction.member.roles.remove ('1202876101005803531');
             interaction.member.roles.add ('1202551817708507136');
-
+            announcementChannel = interaction.guild.channels.cache.get('1202876942714544148');
+            await announcementChannel.send(`${interaction.member} has been smuggled over...`);
+          }
+          else if (userWallet.group === 0) {
+            //group a
+            interaction.member.roles.remove ('1202551817708507136');
+            interaction.member.roles.add ('1202876101005803531');
+            announcementChannel = interaction.guild.channels.cache.get('1202876942714544148');
+            await announcementChannel.send(`${interaction.member} has been smuggled over...`);
           }
 
         // add role.
