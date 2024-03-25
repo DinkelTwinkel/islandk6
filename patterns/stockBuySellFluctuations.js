bdda2011d951aa27a792385d43ed76c3c0a198fe
DinkelTwinkel <128545374+DinkelTwinkel@users.noreply.github.com>
2024-03-25 12:24:01 +0000

    Update stockBuySellFluctuations.js

--- patterns/stockBuySellFluctuations.js
+++ patterns/stockBuySellFluctuations.js
@@ -81,7 +81,6 @@ module.exports = async (client) => {
       const oldPrice = stock.currentValue;
 
       if (interaction.customId === 'buy' + stock.stockName) {
-        await interaction.deferReply({ephemeral: true});
 
         let checkExistingInventory = await Inventory.findOne({
           ownerId: interaction.member.user.id,
@@ -132,7 +131,7 @@ module.exports = async (client) => {
 
         const cost = stock.currentValue;
         const checkPouch = await UserData.findOne ({userID: interaction.member.user.id});
-        if (checkPouch.money < cost) return interaction.editReply ({content: `You do not have enough shells! You need ${cost} shells to perform this action!`, ephemeral: true });
+        if (checkPouch.money < cost) return interaction.reply ({content: `You do not have enough shells! You need ${cost} shells to perform this action!`, ephemeral: true });
         checkPouch.money -= cost;
         await checkPouch.save();
 
@@ -159,11 +158,11 @@ module.exports = async (client) => {
         checkExistingInventory.totalSpent += cost;
 
         if (interaction.channel.id === '1206930735315943444') {
-          interaction.editReply({content: `You bought ${stock.stockName} stock for ${stock.currentValue}, you currently have ${checkExistingInventory.quantity} shares.`, ephemeral: true});
+          interaction.reply({content: `You bought ${stock.stockName} stock for ${stock.currentValue}, you currently have ${checkExistingInventory.quantity} shares.`, ephemeral: true});
           // interaction.deferUpdate();
         }
         else {
-          interaction.editReply({content: `You bought ${stock.stockName} stock for ${stock.currentValue}, you currently have ${checkExistingInventory.quantity} shares.`, ephemeral: true});
+          interaction.reply({content: `You bought ${stock.stockName} stock for ${stock.currentValue}, you currently have ${checkExistingInventory.quantity} shares.`, ephemeral: true});
         }
 
         //refChannel1.send (`${interaction.member.displayName} bought ${stock.stockName} stock for ${stock.currentValue} sea shells, they currently have ${checkExistingInventory.quantity} shares.`);
@@ -193,7 +192,6 @@ module.exports = async (client) => {
 
       }
       else if (interaction.customId === 'sell' + stock.stockName) {
-        await interaction.deferReply({ephemeral: true});
 
                 // check quantity owned and if negative, use short buying logic. 
                 
@@ -244,7 +242,7 @@ module.exports = async (client) => {
         // }
 
 
-        if (!checkExistingInventory || checkExistingInventory.quantity === 0) return  interaction.editReply({content: 'You must buy the stock first!', ephemeral: true });
+        if (!checkExistingInventory || checkExistingInventory.quantity === 0) return interaction.reply({content: 'You must buy the stock first!', ephemeral: true });
         
         // user posesses stock
 
@@ -267,11 +265,11 @@ module.exports = async (client) => {
         await userStat.save();
 
         if (interaction.channel.id === '1206930735315943444') {
-          interaction.editReply({content: `You sold ${stock.stockName} Stock for ${stock.currentValue} shells and paid ${tax} shell in transaction fee, you currently have ${checkExistingInventory.quantity} shares.`, ephemeral: true});
+          interaction.reply({content: `You sold ${stock.stockName} Stock for ${stock.currentValue} shells and paid ${tax} shell in transaction fee, you currently have ${checkExistingInventory.quantity} shares.`, ephemeral: true});
           //interaction.deferUpdate();
         }
         else {
-          interaction.editReply({content: `You sold ${stock.stockName} Stock for ${stock.currentValue} shells and paid ${tax} shell in transaction fee, you currently have ${checkExistingInventory.quantity} shares.`, ephemeral: true});
+          interaction.reply({content: `You sold ${stock.stockName} Stock for ${stock.currentValue} shells and paid ${tax} shell in transaction fee, you currently have ${checkExistingInventory.quantity} shares.`, ephemeral: true});
         }
 
         // const jianDaoWallet = await UserData.findOne({ userID: '1202895682630066216' });
