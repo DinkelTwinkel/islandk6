88014444afa38096040d198b6df13169b550e7ff
DinkelTwinkel <128545374+DinkelTwinkel@users.noreply.github.com>
2024-03-25 11:53:31 +0000

    Update outPutFinalFile.js

--- patterns/outPutFinalFile.js
+++ patterns/outPutFinalFile.js
@@ -25,9 +25,36 @@ module.exports = async (client, channel) => {
         if (statesArray[index].currentState != 'DEAD') {
 
             const data = await UserData.findOne({userID: statesArray[index].userID});
+      
+            let kimoGrade = 'FFF';
+        
+            if (data.kimoScore > 1000) {
+                kimoGrade = 'EEE'
+            }
+            if (data.kimoScore > 3000) {
+                kimoGrade = 'DDD'
+            }
+            if (data.kimoScore > 5000) {
+                kimoGrade = 'CCC'
+            }
+            if (data.kimoScore > 7000) {
+                kimoGrade = 'BBB'
+            }
+            if (data.kimoScore > 10000) {
+                kimoGrade = 'AAA'
+            }
+            if (data.kimoScore > 15000) {
+                kimoGrade = 'SSS'
+            }
+            if (data.kimoScore > 20000) {
+                kimoGrade = 'SSSSS'
+            }
+            if (data.kimoScore > 30000) {
+                kimoGrade = 'EXXXXXX'
+            }
             
             finalString = finalString.concat('=====================\n');
-            finalString = finalString.concat(`${data.name}\n`);
+            finalString = finalString.concat(`${data.name} [GRADE: ${kimoGrade}+]\n`);
             finalString = finalString.concat(`${data.money} Seashells Collected\n`);
             finalString = finalString.concat(`${data.socialLink}\n`);
             finalString = finalString.concat('=====================\n');
